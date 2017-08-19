766f05db7dc41155dd26a3e5e38889df95faaba4
Kata <katalin.molnar.kiss@gmail.com>
2017-08-19 23:33:32 +0200

    some links working already

--- server.py
+++ server.py
@@ -1,28 +1,32 @@
 from flask import Flask, render_template, redirect, request, session
+import csv
+import sys
 
 app = Flask(__name__)
 
-@app.route('/')
-#not specified in the assignment correctly... what is the requirement in the main page???
-#should be a welcoming page, links to /list and /story(add new) can also show /list
-
-@app.route('/list', method=['POST', 'GET'])
+@app.route('/', methods=['GET'])
+@app.route('/list', methods=['GET'])
 #using list.html
 #lists the data in a HTML table
 #the pen at the end of the row redirect to editor, the bin icon removes the actual row from the table
 #has an add new user story button -redirects to the (creating)/story page
-def route_list():
+def route_list(*argv):
+    return render_template('list.html')
 
 
-@app.route('/story')
-#using form.html
-#add new story for the /story page, it is an empty form with a create button
+# @app.route('/story')
+# #using form.html
+# #add new story for the /story page, it is an empty form with a create button
+# def route_story():
+#     return render_template('form.html')
 
-@app.route('/story/<story_id>')
+@app.route('/story/<story_id>', methods=['GET', 'POST'])
 #using editor.html
 #the same form as /story page, but filled in with data of the given User Story
 #Update button should update existing entry, not create a new
+def route_edit(story_id=None):
+    return render_template('form.html', story_id=story_id)
 
 if __name__ == '__main__':
-    app.secret_key = '4Rkj4jo'
-    app.run(debug = True)
+    # app.secret_key = '4Rkj4jo'
+    app.run(debug = True, port=5000)
