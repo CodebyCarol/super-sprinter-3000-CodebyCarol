2f7d280cb4707d0011b19a9b7ede5bb9763a4279
Kata <katalin.molnar.kiss@gmail.com>
2017-08-20 17:23:03 +0200

    csv reading and wrinting is working

--- server.py
+++ server.py
@@ -1,32 +1,49 @@
 from flask import Flask, render_template, redirect, request, session
 import csv
-import sys
+import uuid
+
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 @app.route('/list', methods=['GET'])
-#using list.html
-#lists the data in a HTML table
 #the pen at the end of the row redirect to editor, the bin icon removes the actual row from the table
-#has an add new user story button -redirects to the (creating)/story page
 def route_list(*argv):
-    return render_template('list.html')
+    with open('stories.csv', 'r') as csvfile:
+        rows=[]
+        for row in csvfile:
+            rows.append(row.split(';'))
+    return render_template('list.html', rows=rows)
 
 
-# @app.route('/story')
-# #using form.html
-# #add new story for the /story page, it is an empty form with a create button
-# def route_story():
-#     return render_template('form.html')
+@app.route('/story', methods=['GET', 'POST'])
+#using form.html
+#add new story for the /story page, it is an empty form with a create button
+def route_story():
+    if request.method == 'GET':
+        return render_template('form.html')
 
-@app.route('/story/<story_id>', methods=['GET', 'POST'])
-#using editor.html
-#the same form as /story page, but filled in with data of the given User Story
-#Update button should update existing entry, not create a new
-def route_edit(story_id=None):
-    return render_template('form.html', story_id=story_id)
+    with open('stories.csv', 'a') as csvfile:
+        exportdata = csv.writer(csvfile, delimiter=';',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        exportdata.writerow([uuid.uuid4(), request.form['title'], request.form['descr'], request.form['accr'], request. form['businessvalue'], request. form['estimation'], request. form['status']])
+    
+    return 'Sikeresen feltoltotted a formot!'
+
+
+
+# @app.route('/story/<story_id>', methods=['GET', 'POST'])
+# using form.html
+# the same form as /story page, but filled in with data of the given User Story
+# Update button should update existing entry, not create a new
+# def route_edit(story_id=None):
+#     formdata = cgi.FieldStorage()
+#     title = formdata.getvalue('title')
+#     with open ("sprinter_datas.csv")
+#     return render_template('form.html', story_id=story_id)
+
 
 if __name__ == '__main__':
     # app.secret_key = '4Rkj4jo'
     app.run(debug = True, port=5000)
+
